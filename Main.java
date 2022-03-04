3613b2035659234bcb29aeef5adecb3b225ae779
DianaTurmakhan <97607824+DianaTurmakhan@users.noreply.github.com>
2022-03-04 23:48:03 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -78,3 +78,16 @@ class Solution {
         return arr;
     }
 }
+
+class Solution {
+    public int sumOddLengthSubarrays(int[] arr) {
+        int sum=0;
+        int n=arr.length;
+        for(int len=1;len<=n;len+=2)
+            for(int i=0;i<n-len+1;i++)
+               for(int j=i;j<i+len;j++) 
+                    sum+=arr[j];
+    
+    return sum;
+    }
+}
